8ecd53ca8ee2aa464ff8a067b19ede70b52153ce
Debojit Das <debojit666@gmail.com>
2020-03-04 15:55:02 +0100

    random number generator w-w/o loop

---
+++ random.js
@@ -0,0 +1,17 @@
+var num = 2.50;
+var result = Math.floor(num);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num);
+//2.5 hoile ceiling value nibe
+console.log(result);
+console.log(result2);
+console.log(result3);
+
+
+
+//calculating random number from 0 to 6 (Using loop)
+for(var i=0; i<6; i++){
+    var randomNumber = Math.random() * 6;
+    var output = Math.round(randomNumber);
+    console.log(output);
+}
